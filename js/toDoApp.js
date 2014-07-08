0b1314be131131e44690053ef2513b52f01fe844
rakesh <rakesh.kumar@gmail.com>
2014-07-08 11:14:45 +0530

    .

--- js/toDoApp.js
+++ js/toDoApp.js
@@ -3,7 +3,7 @@ var toDoApp = angular.module('toDoApp',[]);
 
 
 toDoApp.service('listService', function () {
-    this.listService = {};
+//    this.listService = {};
 
     var tasks = [{task:'A', priority:'high',time:'16:40 7 july 2014'}, {task:'B', priority:'medium',time:'14:40 7 july 2014'} ];
 
@@ -14,27 +14,29 @@ toDoApp.service('listService', function () {
 toDoApp.controller('showListsController', function ($scope, listService) {
 
     $scope.taskList = listService;
+
+    $scope.removeTask= function (index) {
+        console.log('remove', index);
+
+       listService.splice(index,1);
+    }
 } );
 
 toDoApp.controller('mainController',function($scope, listService){
+    $scope.priority = 'High';
+    $scope.newTask = 'Enter New Task';
 
    $scope.addTask= function (newTask, priority ) {
        if (newTask) {
            var d = new Date();
-//           console.log('date', d.getDay());
            var taskObj = {
                task: newTask,
                priority: priority,
                time: d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear()
            };
            listService.push(taskObj);
-//           console.log(listService);
 
        }
    }
-    $scope.removeTask= function ($scope) {
-                $scope.listService.splice(index,1);
-    }
-//      console.log($scope.priority);
 
 })
